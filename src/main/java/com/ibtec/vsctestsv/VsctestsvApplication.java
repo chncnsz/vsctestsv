324fe6de2a80a35fc56e098a6b1fb90aa62b48c6
chncnsz <ibrahim.ethem.cansiz@gmail.com>
2022-01-04 17:07:24 +0300

    First Commit

---
+++ src/main/java/com/ibtec/vsctestsv/VsctestsvApplication.java
@@ -0,0 +1,13 @@
+package com.ibtec.vsctestsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsctestsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsctestsvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibtec/vsctestsv/VsctestsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibtec.vsctestsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsctestsvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
